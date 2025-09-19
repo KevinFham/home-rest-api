73298308f572731e81efd68fb903bd532d2e9701
KevinFham <kevin.pha01@gmail.com>
2025-09-19 14:28:58 -0500

    mc server api specification

--- src/api/mc-server/index.ts
+++ src/api/mc-server/index.ts
@@ -11,6 +11,9 @@ enum ServerStatus {
     UNKNOWN = "UNKNOWN",
     ERROR = "ERROR",
 };
+
+const ServerAliasDict = Object.assign({}, ...cfg.mcServer.mcServerAliases.map((key: string) => ({[key]: cfg.mcServer.mcServerContainerNames[cfg.mcServer.mcServerAliases.indexOf(key)]})));
+
 async function getMinecraftServerStatus(serverAddress: string, mcServerContainerName: string): Promise<ServerStatus> {
     const { stdout } = await promiseExec(`ssh -t root@${serverAddress} "docker container inspect -f '{{.State.Status}}, {{.State.Health}} exitcode{{.State.ExitCode}}' ${mcServerContainerName}"`);
     if ( stdout.includes("running") && stdout.includes("healthy") ) {
@@ -60,69 +63,102 @@ const actionHandler = async ( req: Request, res: Response ) => {
         }
     }
 
-    else if ( payload.action === 'startMinecraftServer' ) {                         // Start Minecraft Server
+    else if ( payload.action === 'getMinecraftServerList' ) {
         const isUp = await isMachineUp(cfg.mcServer.serverHostName);
-        if ( isUp ) {
-            const mcStatus = await getMinecraftServerStatus(cfg.mcServer.serverHostName, cfg.mcServer.mcServerContainerName);
-            if ( mcStatus === ServerStatus.ACTIVE ) {
-                res.status(200).send({ code: 1, message: "Minecraft server is already up and running!" });
-            } else if ( mcStatus === ServerStatus.STARTING ) {
-                res.status(200).send({ code: 1, message: "Minecraft server is already starting!" });
-            } else if ( mcStatus === ServerStatus.STOPPED || mcStatus === ServerStatus.ERROR ) {
-                await promiseExec(`ssh -t root@${cfg.mcServer.serverHostName} "docker start ${cfg.mcServer.mcServerContainerName}"`); 
-                res.status(200).send({ code: 0, message: "Starting Minecraft Server!" });
-            } else {
-                res.status(200).send({ code: 1, message: "Minecraft server status unknown." });
-            }
+         if ( isUp ) {
+             res.status(200).send({ code: 0, message: JSON.stringify({
+                 mcServerAliases: cfg.mcServer.mcServerAliases,
+                 mcServerAddrs: cfg.mcServer.mcServerAddrs
+             }) });
+         } else {
+             res.status(200).send({ code: 1, message: "Machine is down. Unable to get server list" });
+         }
+    }
 
-        } else {
-            res.status(200).send({ code: 1, message: "Server is down because machine is down!" });
+    else if ( payload.action === 'startMinecraftServer' ) {                         // Start Minecraft Server
+        if ( 'mcInstance' in payload && payload.mcInstance in ServerAliasDict ) { 
+            const isUp = await isMachineUp(cfg.mcServer.serverHostName);
+            if ( isUp ) {
+                const mcStatus = await getMinecraftServerStatus(cfg.mcServer.serverHostName, ServerAliasDict[payload.mcInstance]);
+                if ( mcStatus === ServerStatus.ACTIVE ) {
+                    res.status(200).send({ code: 1, message: "Minecraft server is already up and running!" });
+                } else if ( mcStatus === ServerStatus.STARTING ) {
+                    res.status(200).send({ code: 1, message: "Minecraft server is already starting!" });
+                } else if ( mcStatus === ServerStatus.STOPPED || mcStatus === ServerStatus.ERROR ) {
+                    await promiseExec(`ssh -t root@${cfg.mcServer.serverHostName} "docker start ${ServerAliasDict[payload.mcInstance]}"`); 
+                    res.status(200).send({ code: 0, message: "Starting Minecraft Server!" });
+                } else {
+                    res.status(200).send({ code: 1, message: "Minecraft server status unknown." });
+                }
+
+            } else {
+                res.status(200).send({ code: 1, message: "Server is down because machine is down!" });
+            }
+        }
+        else if ( 'mcInstance' in payload && !( payload.mcInstance in ServerAliasDict ) )
+            res.status(200).send({ code: 1, message: `${payload.mcInstance} not recognized in server aliases`});
+        else {
+            res.status(200).send({ code: 1, message: `Missing "mcInstance" parameter` });
         }
     }
 
     else if ( payload.action === 'getMinecraftServerStatus' ) {                     // Get Minecraft Server Status
-        var isUp = await isMachineUp(cfg.mcServer.serverHostName);
-        if ( isUp ) {
-            const mcStatus = await getMinecraftServerStatus(cfg.mcServer.serverHostName, cfg.mcServer.mcServerContainerName);
-            if ( mcStatus === ServerStatus.ACTIVE ) {
-                const currentlyOnline = await getMinecraftServerPlayers(cfg.mcServer.serverHostName, cfg.mcServer.mcServerContainerName);
-                res.status(200).send({ code: 0, serverStat: "running", players: currentlyOnline, message: "Minecraft server is up and running!" });
-            } else if ( mcStatus === ServerStatus.STARTING ) {
-                res.status(200).send({ code: 0, serverStat: "starting", players: [], message: "Minecraft server is starting!" });
-            } else if ( mcStatus === ServerStatus.ERROR ){
-                res.status(200).send({ code: 1, serverStat: "error", players: [], message: "Minecraft server has an error!" });
-            } else if ( mcStatus === ServerStatus.STOPPED ) {
-                res.status(200).send({ code: 0, serverStat: "exited", players: [], message: "Minecraft server is shut down!" });
+        if ( 'mcInstance' in payload && payload.mcInstance in ServerAliasDict ) { 
+            var isUp = await isMachineUp(cfg.mcServer.serverHostName);
+            if ( isUp ) {
+                const mcStatus = await getMinecraftServerStatus(cfg.mcServer.serverHostName, ServerAliasDict[payload.mcInstance]);
+                if ( mcStatus === ServerStatus.ACTIVE ) {
+                    const currentlyOnline = await getMinecraftServerPlayers(cfg.mcServer.serverHostName, ServerAliasDict[payload.mcInstance]);
+                    res.status(200).send({ code: 0, serverStat: "running", players: currentlyOnline, message: "Minecraft server is up and running!" });
+                } else if ( mcStatus === ServerStatus.STARTING ) {
+                    res.status(200).send({ code: 0, serverStat: "starting", players: [], message: "Minecraft server is starting!" });
+                } else if ( mcStatus === ServerStatus.ERROR ){
+                    res.status(200).send({ code: 1, serverStat: "error", players: [], message: "Minecraft server has an error!" });
+                } else if ( mcStatus === ServerStatus.STOPPED ) {
+                    res.status(200).send({ code: 0, serverStat: "exited", players: [], message: "Minecraft server is shut down!" });
+                } else {
+                    res.status(200).send({ code: 1, serverStat: "unknown", players: [], message: "Minecraft server status unknown." });
+                }
             } else {
-                res.status(200).send({ code: 1, serverStat: "unknown", players: [], message: "Minecraft server status unknown." });
+                res.status(200).send({ code: 1, serverStat: "down", players: [], message: "Server is down because machine is down!" });
             }
-        } else {
-            res.status(200).send({ code: 1, serverStat: "down", players: [], message: "Server is down because machine is down!" });
+        }
+        else if ( 'mcInstance' in payload && !( payload.mcInstance in ServerAliasDict ) )
+            res.status(200).send({ code: 1, message: `${payload.mcInstance} not recognized in server aliases`});
+        else {
+            res.status(200).send({ code: 1, message: `Missing "mcInstance" parameter` });
         }
     }
 
     else if ( payload.action === 'stopMinecraftServer' ) {                          // Stop Minecraft Server
-        const isUp = await isMachineUp(cfg.mcServer.serverHostName);
-        if ( isUp ) {
-            const mcStatus = await getMinecraftServerStatus(cfg.mcServer.serverHostName, cfg.mcServer.mcServerContainerName);
-            if ( mcStatus === ServerStatus.ACTIVE ) {
-                const currentlyOnline = await getMinecraftServerPlayers(cfg.mcServer.serverHostName, cfg.mcServer.mcServerContainerName);
-                if ( !( currentlyOnline.length > 0 ) ) {
-                    await promiseExec(`ssh -t root@${cfg.mcServer.serverHostName} "docker exec ${cfg.mcServer.mcServerContainerName} rcon-cli \"stop\""`);
-                    res.status(200).send({ code: 0, message: "Shutting down Minecraft Server..." });
-                } else {
-                    res.status(200).send({ code: 1, message: `${currentlyOnline.length} players online! Aborting shutdown.` });
-                }
+        if ( 'mcInstance' in payload && payload.mcInstance in ServerAliasDict ) { 
+            const isUp = await isMachineUp(cfg.mcServer.serverHostName);
+            if ( isUp ) {
+                const mcStatus = await getMinecraftServerStatus(cfg.mcServer.serverHostName, ServerAliasDict[payload.mcInstance]);
+                if ( mcStatus === ServerStatus.ACTIVE ) {
+                    const currentlyOnline = await getMinecraftServerPlayers(cfg.mcServer.serverHostName, ServerAliasDict[payload.mcInstance]);
+                    if ( !( currentlyOnline.length > 0 ) ) {
+                        await promiseExec(`ssh -t root@${cfg.mcServer.serverHostName} "docker exec ${ServerAliasDict[payload.mcInstance]} rcon-cli \"stop\""`);
+                        res.status(200).send({ code: 0, message: "Shutting down Minecraft Server..." });
+                    } else {
+                        res.status(200).send({ code: 1, message: `${currentlyOnline.length} players online! Aborting shutdown.` });
+                    }
 
-            } else if ( mcStatus === ServerStatus.STARTING ) {
-                res.status(200).send({ code: 1, message: "Minecraft server is starting up. Please wait until server is fully up and running." });
-            } else if ( mcStatus === ServerStatus.STOPPED || mcStatus === ServerStatus.ERROR ) {
-                res.status(200).send({ code: 1, message: "Minecraft server is already stopped!" });
+                } else if ( mcStatus === ServerStatus.STARTING ) {
+                    res.status(200).send({ code: 1, message: "Minecraft server is starting up. Please wait until server is fully up and running." });
+                } else if ( mcStatus === ServerStatus.STOPPED || mcStatus === ServerStatus.ERROR ) {
+                    res.status(200).send({ code: 1, message: "Minecraft server is already stopped!" });
+                } else {
+                    res.status(200).send({ code: 1, message: "Minecraft server status unknown." });
+                }
             } else {
-                res.status(200).send({ code: 1, message: "Minecraft server status unknown." });
+                res.status(200).send({ code: 1, message: "Server is already down because machine is down" });
             }
-        } else {
-            res.status(200).send({ code: 1, message: "Server is already down because machine is down" });
+        }
+        else if ( 'mcInstance' in payload && !( payload.mcInstance in ServerAliasDict ) )
+            res.status(200).send({ code: 1, message: `${payload.mcInstance} not recognized in server aliases`});
+        else {
+            res.status(200).send({ code: 1, message: `Missing "mcInstance" parameter` });
         }
     }
 
