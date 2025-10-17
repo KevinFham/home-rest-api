35c3110ece47c383a206ee908eb00c26d42b1da5
KevinFham <kevin.pha01@gmail.com>
2025-10-17 16:07:49 -0500

    vps tests later

--- tests/api/vps.test.ts
+++ tests/api/vps.test.ts
@@ -1,7 +1,8 @@
 import { beforeAll, vi } from 'vitest';
-import { VPSController } from '../../src/api/vps/utils.js';
+import { GET, PUT } from '../../src/api/vps/index.ts';
+import { mockRequest, mockResponse } from '../express-mocks.ts';
 
-//TODO: Flesh out VPS Utils test
+// TODO: Flesh out VPS tests
 
 beforeAll(() => {
     vi.mock('../../src/utils.js', async (importOriginal) => {
@@ -23,12 +24,19 @@ beforeAll(() => {
             DoctlContext: actual.DoctlContext,
         }
     });
+
+    vi.mock('../../src/api/vps/utils.js', async (importOriginal) => {
+        const actual = await importOriginal();
+        return {
+            VPSController: actual.VPSController,
+        }
+    });
 });
 
-test('Basic Control', async () => {
+test('GET', async () => {
     expect(true).toBeTruthy();
 });
 
-test('Timers', async () => {
+test('PUT', async () => {
     expect(true).toBeTruthy();
 });
